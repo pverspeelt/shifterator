143eaae503c8c6e030b3f65e6cf72b306add0021
pverspeelt <phiver4@gmail.com>
2020-12-30 18:43:07 +0100

    FEAT: main theme created
    Adds the main_theme in utils.R.
    Adds ggplot2 to Imports in the description.

--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,13 @@ stop_quietly <- function() {
   on.exit(options(opt))
   stop()
 }
+
+
+main_theme <- function(...){
+  ggplot2::theme_grey() +
+    ggplot2::theme(panel.border = ggplot2::element_rect(fill = NA, size = 1, colour = "grey"), 
+                   panel.grid = ggplot2::element_line(colour = "grey"),
+                   panel.grid.minor.x = ggplot2::element_line(color = NA),
+                   panel.grid.minor.y = ggplot2::element_line(color = NA),
+                   panel.background = ggplot2::element_rect(fill = NA))
+}
