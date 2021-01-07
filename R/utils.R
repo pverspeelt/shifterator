e57d3435a3caca13c6198f6a6c4fd1daa97e3a59
pverspeelt <phiver4@gmail.com>
2021-01-07 20:23:23 +0100

    Feat: adds a function for creating the bar colours.
    Adds get_bar_colours to the utils file.
    Adds a hidden list object to the utils file that contains the colours to be used in plotting.

--- R/utils.R
+++ R/utils.R
@@ -35,3 +35,71 @@ text_size_theme <- function(...){
 scientific_to_10 <- function(x){
   parse(text=gsub("1e\\+*", " 10^", scales::scientific_format()(x))) 
 }
+
+
+#' Get bar colours for plotting
+#' 
+#' Returns the component colours of each type's contribution bars.
+#'
+#' @param top_shift_scores top n values of the shift_scores data.frame from a 
+#' shift object
+#' @param all_pos_contributions Defaults to FALSE when calling get_shift_graphs.
+#'
+#' @return Returns a list with 3 values. The values are lists of the colors 
+#' to assign to the p_diff and s_diff components respectively. If just the 
+#' overall contributions are being shown in a simple (not detailed) shift graph, 
+#' then the "total" colours are used.
+#' 
+#' @keywords internal
+#' 
+get_bar_colours <- function(top_shift_scores, all_pos_contributions){
+  c_total = top_shift_scores$type2p_diff * top_shift_scores$type2s_ref_diff + 
+    top_shift_scores$type2p_avg * top_shift_scores$type2s_diff
+  # Get total contribution colors
+  if(!all_pos_contributions == TRUE){
+    bar_colours_total <- ifelse(c_total > 0, 
+                                .score_colours[["pos_total"]], 
+                                .score_colours[["neg_total"]])
+  } else {
+    bar_colours_total <- ifelse(top_shift_scores$type2p_diff > 0, 
+                                .score_colours[["all_pos_pos"]], 
+                                .score_colours[["all_pos_neg"]])
+  }
+  # Get p_diff * s_ref_diff comp colors
+  bar_colours_p <- ifelse(top_shift_scores$type2s_ref_diff > 0 & top_shift_scores$type2p_diff > 0, 
+                          .score_colours[["pos_s_pos_p"]], 
+                          "")
+  bar_colours_p <- ifelse(top_shift_scores$type2s_ref_diff > 0 & top_shift_scores$type2p_diff <= 0, 
+                          .score_colours[["pos_s_neg_p"]], 
+                          bar_colours_p)
+  bar_colours_p <- ifelse(top_shift_scores$type2s_ref_diff <= 0 & top_shift_scores$type2p_diff > 0, 
+                          .score_colours[["neg_s_pos_p"]], 
+                          bar_colours_p)
+  bar_colours_p <- ifelse(top_shift_scores$type2s_ref_diff <= 0 & top_shift_scores$type2p_diff <= 0, 
+                          .score_colours[["neg_s_neg_p"]], 
+                          bar_colours_p)
+  
+  # Get s_diff comp colors
+  bar_colours_s <- ifelse(top_shift_scores$type2s_diff > 0, 
+                          .score_colours[["pos_s"]], 
+                          .score_colours[["neg_s"]])
+  
+  bar_colours <- list(bar_colours_total = bar_colours_total,
+                      bar_colours_p = bar_colours_p,
+                      bar_colours_s = bar_colours_s)
+  
+  bar_colours
+}
+
+.score_colours <- list(
+  all_pos_neg = "#9E75B7",
+  all_pos_pos = "#FECC5D",
+  neg_s = "#9E75B7",
+  neg_s_neg_p = "#C4CAFC",
+  neg_s_pos_p = "#2F7CCE",
+  neg_total = "#9E75B7",
+  pos_s = "#FECC5D",
+  pos_s_neg_p = "#FDFFD2",
+  pos_s_pos_p = "#FFFF80",
+  pos_total = "#FECC5D",
+  total = "#707070")
